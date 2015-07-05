87aad343aea5232418388d34851ee597a89c8e7c
Jan Horava <czechinthemail@gmail.com>
2015-07-05 16:42:00 -0700

    moved buffer display to css file

--- js/shooter-2d.js
+++ js/shooter-2d.js
@@ -606,7 +606,7 @@ function setmode(newmode, newgame){
         load_level(mode);
 
         if(newgame){
-            document.getElementById('page').innerHTML = '<canvas id=canvas oncontextmenu="return false"></canvas><canvas id=buffer style=display:none></canvas>';
+            document.getElementById('page').innerHTML = '<canvas id=canvas oncontextmenu="return false"></canvas><canvas id=buffer></canvas>';
 
             buffer = document.getElementById('buffer').getContext('2d');
             canvas = document.getElementById('canvas').getContext('2d');
