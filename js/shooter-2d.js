926719b9e4853c957df1b9dbbbfe1e942f303c06
honzi <czechinthemail@gmail.com>
2016-05-01 16:17:51 -0700

    added targetting line to player

--- js/shooter-2d.js
+++ js/shooter-2d.js
@@ -60,7 +60,7 @@ function draw_logic(){
         }
     }
 
-    // Draw player.
+    // Draw player and targeting direction.
     buffer.fillStyle = settings['color'];
     buffer.fillRect(
       -17,
@@ -68,6 +68,25 @@ function draw_logic(){
       34,
       34
     );
+    var endpoint = get_fixed_length_line(
+      0,
+      0,
+      mouse_x - x,
+      mouse_y - y,
+      25
+    );
+    buffer.beginPath();
+    buffer.moveTo(
+      0,
+      0
+    );
+    buffer.lineTo(
+      endpoint['x'],
+      endpoint['y']
+    );
+    buffer.closePath();
+    buffer.strokeStyle = '#fff';
+    buffer.stroke();
 
     // Restore buffer.
     buffer.restore();
@@ -140,6 +159,28 @@ function draw_logic(){
           100
         );
     }
+}
+
+function get_fixed_length_line(x0, y0, x1, y1, length){
+    var distance = Math.sqrt(
+      Math.pow(
+        x1 - x0,
+        2
+      ) + Math.pow(
+        y1 - y0,
+        2
+      )
+    );
+
+    x1 /= distance;
+    x1 *= length;
+    y1 /= distance;
+    y1 *= length;
+
+    return {
+      'x': x1,
+      'y': y1,
+    };
 }
 
 function get_movement_speed(x0, y0, x1, y1){
